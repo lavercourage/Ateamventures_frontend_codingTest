5b02fce0d0666af162aee8d6a5619bb44dfff37c
lavercourage <smqkemfht@gmail.com>
2022-03-07 03:35:37 +0900

    220307 : [modify] 드롭다운 css 수정, 하위목록 체크박스 추가

--- capa_partners/src/components/dropdown/material/MaterialDropdown.tsx
+++ capa_partners/src/components/dropdown/material/MaterialDropdown.tsx
@@ -41,5 +41,5 @@ const MaterialDropdown = () => {
 export default MaterialDropdown;
 
 // 체크 선택 시 state값 변화를 줘서 배경 색 변경
-// 선택한 체크 개수만큼 메뉴바에 숫자 적어넣기 { aaa ? {~}개 : <></>}
+// 선택한 체크 개수만큼 메뉴바에 숫자 적어넣기 { aaa ? `{~}개` : <></>}
 // 선택한 체크가 있을 시 우측에 리셋버튼 놓기 { aaa ? 필터링 리셋 : <></>}
