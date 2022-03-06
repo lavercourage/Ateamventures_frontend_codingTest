d7cb35a8c59d9147f7744eab46316dc8a533c1a2
lavercourage <smqkemfht@gmail.com>
2022-03-07 03:31:50 +0900

    220307 : [modify] 드롭박스 기능 수정

--- capa_partners/src/components/dropdown/material/MaterialDropdown.tsx
+++ capa_partners/src/components/dropdown/material/MaterialDropdown.tsx
@@ -1,26 +1,35 @@
-import { Menu, Dropdown, Button, message, Space, Tooltip } from "antd";
+import { Menu, Dropdown, Button, Checkbox } from "antd";
 import { CaretDownOutlined } from "@ant-design/icons";
 
+// 드롭다운
 const handleButtonClick = (e: any) => {
-  message.info("Click on left button.");
+  // message.info("Click on left button.");
   console.log("click left button", e);
 };
 
 const handleMenuClick = (e: any) => {
-  message.info("Click on menu item.");
   console.log("click", e);
+};
+
+// 체크
+const onChange = (e: any) => {
+  console.log(`checked = ${e.target.checked}`);
 };
 
 const menu = (
   <Menu onClick={handleMenuClick}>
-    <Menu.Item key="1">밀링</Menu.Item>
-    <Menu.Item key="2">선반</Menu.Item>
+    <Menu.Item key="1">
+      <Checkbox onChange={onChange}>밀링</Checkbox>
+    </Menu.Item>
+    <Menu.Item key="2">
+      <Checkbox onChange={onChange}>선반</Checkbox>
+    </Menu.Item>
   </Menu>
 );
 
 const MaterialDropdown = () => {
   return (
-    <Dropdown overlay={menu} overlayStyle={{ width: "98px", height: "32px" }}>
+    <Dropdown overlay={menu} trigger={["click"]}>
       <Button>
         가공방식
         <CaretDownOutlined />
@@ -30,3 +39,7 @@ const MaterialDropdown = () => {
 };
 
 export default MaterialDropdown;
+
+// 체크 선택 시 state값 변화를 줘서 배경 색 변경
+// 선택한 체크 개수만큼 메뉴바에 숫자 적어넣기 { aaa ? {~}개 : <></>}
+// 선택한 체크가 있을 시 우측에 리셋버튼 놓기 { aaa ? 필터링 리셋 : <></>}
--- capa_partners/src/components/dropdown/method/MethodDropdown.tsx
+++ capa_partners/src/components/dropdown/method/MethodDropdown.tsx
@@ -1,31 +1,47 @@
 import styled from "@emotion/styled";
-import { Menu, Dropdown, Button, message, Space, Tooltip } from "antd";
+import { Menu, Dropdown, Button, Checkbox } from "antd";
 import { CaretDownOutlined } from "@ant-design/icons";
 
+// 드롭다운
 const handleButtonClick = (e: any) => {
-  message.info("Click on left button.");
+  // message.info("Click on left button.");
   console.log("click left button", e);
 };
 
 const handleMenuClick = (e: any) => {
-  message.info("Click on menu item.");
+  // message.info("Click on menu item.");
   console.log("click", e);
+};
+
+// 체크
+const onChange = (e: any) => {
+  console.log(`checked = ${e.target.checked}`);
 };
 
 const menu = (
   <Menu onClick={handleMenuClick}>
-    <Menu.Item key="1">알루미늄</Menu.Item>
-    <Menu.Item key="2">탄소강</Menu.Item>
-    <Menu.Item key="3">구리</Menu.Item>
-    <Menu.Item key="4">합금강</Menu.Item>
-    <Menu.Item key="5">강철</Menu.Item>
+    <Menu.Item key="1">
+      <Checkbox onChange={onChange}>알루미늄</Checkbox>
+    </Menu.Item>
+    <Menu.Item key="2">
+      <Checkbox onChange={onChange}>탄소강</Checkbox>
+    </Menu.Item>
+    <Menu.Item key="3">
+      <Checkbox onChange={onChange}>구리</Checkbox>
+    </Menu.Item>
+    <Menu.Item key="4">
+      <Checkbox onChange={onChange}>합금강</Checkbox>
+    </Menu.Item>
+    <Menu.Item key="5">
+      <Checkbox onChange={onChange}>강철</Checkbox>
+    </Menu.Item>
   </Menu>
 );
 
 const MethodDropdown = () => {
   return (
     <DropdownWrapper>
-      <Dropdown overlay={menu}>
+      <Dropdown overlay={menu} trigger={["click"]}>
         <Button>
           재료
           <CaretDownOutlined />
