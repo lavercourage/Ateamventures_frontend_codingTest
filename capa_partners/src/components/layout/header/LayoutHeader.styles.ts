6836f956a301b99e61cc9357b7b3fb95f750a9c0
lavercourage <smqkemfht@gmail.com>
2022-03-09 06:17:45 +0900

    220309 : [update] 빌딩아이콘 및 로고 이미지 색상별 추가 [modify] 헤더 반응형 수정

--- capa_partners/src/components/layout/header/LayoutHeader.styles.ts
+++ capa_partners/src/components/layout/header/LayoutHeader.styles.ts
@@ -17,12 +17,20 @@ export const Wrapper = styled.header`
   }
 `;
 
-export const MenuIcon = styled.img`
+export const LeftWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const MenuIcon = styled.img<{ isOpen: boolean }>`
   display: none;
 
   @media ${breakPoints.mobile} {
     height: 20px;
     display: flex;
+    :hover {
+      cursor: pointer;
+    }
   }
 `;
 
@@ -38,11 +46,19 @@ export const Logo = styled.img`
 export const Menu = styled.span`
   display: flex;
   align-items: center;
+
+  @media ${breakPoints.mobile} {
+    display: none;
+  }
 `;
 
 export const TitleWrapper = styled.span`
   display: flex;
   align-items: center;
+
+  @media ${breakPoints.mobile} {
+    margin-bottom: 24px;
+  }
 `;
 
 export const BuildIcon = styled.img`
@@ -56,6 +72,18 @@ export const Title = styled.span`
   :hover {
     cursor: pointer;
   }
+
+  @media ${breakPoints.mobile} {
+    display: none;
+  }
+`;
+
+export const TitleMobile = styled.span`
+  color: #000000;
+  font-size: 14px;
+  :hover {
+    cursor: pointer;
+  }
 `;
 
 export const Bar = styled.div`
@@ -63,4 +91,59 @@ export const Bar = styled.div`
   height: 16px;
   margin: 0 32px;
   background-color: #ffffff;
+
+  @media ${breakPoints.mobile} {
+    width: 100%;
+    height: 1px;
+    margin: 0;
+    background-color: none;
+    border-top: 1px solid #e5e5e5;
+  }
+`;
+
+export const AAA = styled.div<{ isOpen: boolean }>`
+  display: none;
+
+  @media ${breakPoints.mobile} {
+    width: 100%;
+    height: 100vh;
+    z-index: 1;
+    display: flex;
+    justify-content: space-between;
+    transition: all 0.4s;
+    position: fixed;
+    top: 0;
+    left: ${(props) => (props.isOpen ? 0 : "-360px")};
+  } ;
+`;
+
+export const BBB = styled.div`
+  @media ${breakPoints.mobile} {
+    width: 280px;
+    height: 100%;
+    background-color: #ffffff;
+  }
+`;
+
+export const CCC = styled.div<{ isOpen: boolean }>`
+  width: calc(100vw - 280px);
+  height: 100%;
+  background-color: #000000;
+  opacity: 0.5;
+  transition: all 0.4s;
+  position: fixed;
+  top: 0;
+  right: ${(props) => (props.isOpen ? `calc(-100vw)` : 0)};
+`;
+
+export const LogoMobile = styled.img`
+  height: 12px;
+  margin: 16px 20px;
+`;
+
+export const DDD = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 36px;
+  margin-left: 32px;
 `;
--- capa_partners/src/components/layout/header/LayoutHeader.tsx
+++ capa_partners/src/components/layout/header/LayoutHeader.tsx
@@ -1,19 +1,54 @@
 import * as H from "./LayoutHeader.styles";
+import { useState } from "react";
 
 const LayoutHeader = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClickMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <H.Wrapper>
-      <H.MenuIcon src="/images/Menu.svg" />
-      <H.Logo src="/images/MainLogo.svg" />
-      <H.Menu>
-        <H.TitleWrapper>
-          <H.BuildIcon src="/images/Build.svg" />
-          <H.Title style={{ fontFamily: "NotoSansKRM" }}>A 가공 업체</H.Title>
-        </H.TitleWrapper>
-        <H.Bar />
-        <H.Title style={{ fontFamily: "NotoSansKRR" }}>로그아웃</H.Title>
-      </H.Menu>
-    </H.Wrapper>
+    <>
+      <H.AAA isOpen={isOpen}>
+        <H.BBB>
+          <H.LogoMobile src="/images/MainLogoBlue.svg" />
+          <H.Bar />
+          <H.DDD>
+            <H.TitleWrapper>
+              <H.BuildIcon src="/images/BuildBlack.svg" />
+              <H.TitleMobile style={{ fontFamily: "NotoSansKRM" }}>
+                A 가공 업체
+              </H.TitleMobile>
+            </H.TitleWrapper>
+            <H.TitleMobile style={{ fontFamily: "NotoSansKRR" }}>
+              로그아웃
+            </H.TitleMobile>
+          </H.DDD>
+        </H.BBB>
+        <H.CCC onClick={onClickMenu} isOpen={!isOpen} />
+      </H.AAA>
+      {/*  */}
+      {/*  */}
+      <H.Wrapper>
+        <H.LeftWrapper>
+          <H.MenuIcon
+            onClick={onClickMenu}
+            isOpen={isOpen}
+            src="/images/Menu.svg"
+          />
+          <H.Logo src="/images/MainLogoWhite.svg" />
+        </H.LeftWrapper>
+        <H.Menu>
+          <H.TitleWrapper>
+            <H.BuildIcon src="/images/BuildWhite.svg" />
+            <H.Title style={{ fontFamily: "NotoSansKRM" }}>A 가공 업체</H.Title>
+          </H.TitleWrapper>
+          <H.Bar />
+          <H.Title style={{ fontFamily: "NotoSansKRR" }}>로그아웃</H.Title>
+        </H.Menu>
+      </H.Wrapper>
+    </>
   );
 };
 
